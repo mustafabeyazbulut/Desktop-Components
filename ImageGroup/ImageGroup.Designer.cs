3289f74c8b86d58885cd31cc146db2cfb220914f
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2024-02-15 15:40:39 +0300

    ImageGroup Component
    ImageGroup Component

---
+++ ImageGroup/ImageGroup.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class ImageGroup
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ovalPictureBox1 = new OvalPictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ovalPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(0, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 63);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ImageGroup";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseLeave += new System.EventHandler(this.label1_MouseLeave);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 18);
+            this.label2.TabIndex = 2;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.MouseLeave += new System.EventHandler(this.label2_MouseLeave);
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label2_MouseMove);
+            // 
+            // ovalPictureBox1
+            // 
+            this.ovalPictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.ovalPictureBox1.BackColor = System.Drawing.Color.White;
+            this.ovalPictureBox1.Location = new System.Drawing.Point(15, 10);
+            this.ovalPictureBox1.Name = "ovalPictureBox1";
+            this.ovalPictureBox1.Size = new System.Drawing.Size(119, 78);
+            this.ovalPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ovalPictureBox1.TabIndex = 0;
+            this.ovalPictureBox1.TabStop = false;
+            this.ovalPictureBox1.Click += new System.EventHandler(this.ovalPictureBox1_Click);
+            this.ovalPictureBox1.MouseLeave += new System.EventHandler(this.ovalPictureBox1_MouseLeave);
+            this.ovalPictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ovalPictureBox1_MouseMove);
+            // 
+            // ImageGroup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ovalPictureBox1);
+            this.Controls.Add(this.label2);
+            this.Name = "ImageGroup";
+            this.Load += new System.EventHandler(this.ImageGroup_Load);
+            this.MouseLeave += new System.EventHandler(this.ImageGroup_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ImageGroup_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.ovalPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OvalPictureBox ovalPictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
---
+++ ImageGroup/ImageGroup.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace WindowsFormsApplication1
+{
+    public partial class ImageGroup : UserControl
+    {
+        private const int shadowOffset = 5;
+
+        public ImageGroup()
+        {
+            InitializeComponent();
+            this.Resize += new EventHandler(ImageGroup_Resize);
+        }
+
+        private void ImageGroup_Load(object sender, EventArgs e)
+        {
+            RoundCorners();
+        }
+
+        private void ImageGroup_Resize(object sender, EventArgs e)
+        {
+            RoundCorners();
+        }
+
+        private void RoundCorners()
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddLine(radius, 0, this.Width - radius, 0);
+            path.AddArc(this.Width - radius, 0, radius, radius, 270, 90);
+            path.AddLine(this.Width, radius, this.Width, this.Height - radius);
+            path.AddArc(this.Width - radius, this.Height - radius, radius, radius, 0, 90);
+            path.AddLine(this.Width - radius, this.Height, radius, this.Height);
+            path.AddArc(0, this.Height - radius, radius, radius, 90, 90);
+            path.AddLine(0, this.Height - radius, 0, radius);
+            path.AddArc(0, 0, radius, radius, 180, 90);
+            this.Region = new Region(path);
+        }
+
+        #region  İmport
+
+        private int radius = 50;
+        [Category("Özellikler")]
+        [Browsable(true)]
+        public int Radius
+        {
+            get { return radius; }
+            set
+            {
+                radius = value;
+                ImageGroup_Resize(this, EventArgs.Empty);
+            }
+        }
+
+        private int labelSize = 15;
+        [Category("Özellikler")]
+        [Browsable(true)]
+        public int LabelSize
+        {
+            get { return labelSize; }
+            set
+            {
+                labelSize = value;
+                label1.Font = new Font(label1.Font.FontFamily, labelSize);
+            }
+        }
+
+        private Image resim;
+        [Category("Özellikler")]
+        [Browsable(true)]
+        public Image Resim
+        {
+            get { return resim; }
+            set
+            {
+                resim = value;
+                ovalPictureBox1.Image = resim;
+            }
+        }
+
+        private string text = "ImageGroup";
+        [Category("Özellikler")]
+        [Browsable(true)]
+        public string Texti
+        {
+            get { return text; }
+            set
+            {
+                text = value;
+                label1.Text = text;
+            }
+        }
+        private Color imageBackColor = Color.White;
+        [Category("Özellikler")]
+        [Browsable(true)]
+        public Color ImageBackColor
+        {
+            get { return imageBackColor; }
+            set
+            {
+                imageBackColor = value;
+                ovalPictureBox1.BackColor = value;
+            }
+        }
+
+        private Color imageGroupBackColor = Color.White;
+        [Category("Özellikler")]
+        [Browsable(true)]
+        public Color ImageGroupBackColor
+        {
+            get { return imageGroupBackColor; }
+            set
+            {
+                imageGroupBackColor = value;
+                this.BackColor = value;
+            }
+        }
+        private Color imageGroupHoverColor = Color.Red;
+        [Category("Özellikler")]
+        [Browsable(true)]
+        public Color ImageGroupHoverColor
+        {
+            get { return imageGroupHoverColor; }
+            set
+            {
+                imageGroupHoverColor = value;
+            }
+        }
+
+        private Color textColor = Color.Black; // or any default color
+        [Category("Özellikler")]
+        [Browsable(true)]
+        public Color TextColor
+        {
+            get { return textColor; }
+            set
+            {
+                textColor = value;
+                label1.ForeColor = value;
+            }
+        }
+        private PictureBoxSizeMode pictureBoxSizeMode = PictureBoxSizeMode.Zoom;
+        [Category("Özellikler")]
+        [Browsable(true)]
+        public PictureBoxSizeMode PictureBoxSizeMode
+        {
+            get { return pictureBoxSizeMode; }
+            set
+            {
+                pictureBoxSizeMode = value;
+                ovalPictureBox1.SizeMode = pictureBoxSizeMode;
+            }
+        }
+       
+        #endregion
+
+        #region mause event
+        private void ChangeColors(Color color)
+        {
+            this.BackColor = color;
+            ovalPictureBox1.BackColor = color;
+            label1.BackColor = color;
+            if (imageGroupBackColor==color)
+            {
+                ovalPictureBox1.BackColor = imageBackColor;
+            }
+        }
+        private void ImageGroup_MouseMove(object sender, MouseEventArgs e)
+        {
+            ChangeColors(imageGroupHoverColor);
+        }
+
+        private bool IsMouseOverControl(Control control)
+        {
+            Point localCursor = control.PointToClient(Cursor.Position);
+            return control.ClientRectangle.Contains(localCursor);
+        }
+        private void ImageGroup_MouseLeave(object sender, EventArgs e)
+        {
+            if (IsMouseOverControl(this))
+            {
+                ChangeColors(imageGroupHoverColor);
+            }
+            else
+            {
+                ChangeColors(imageGroupBackColor);
+            }
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            ChangeColors(imageGroupHoverColor);
+
+        }
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            ChangeColors(imageGroupBackColor);
+
+        }
+
+        private void ovalPictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            ChangeColors(imageGroupHoverColor);
+
+        }
+
+        private void ovalPictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            ChangeColors(imageGroupBackColor);
+
+        }
+
+        private void label2_MouseLeave(object sender, EventArgs e)
+        {
+            ChangeColors(imageGroupBackColor);
+
+        }
+
+        private void label2_MouseMove(object sender, MouseEventArgs e)
+        {
+            ChangeColors(imageGroupHoverColor);
+
+        }
+        private void ovalPictureBox1_Click(object sender, EventArgs e)
+        {
+            OnClick(e);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            OnClick(e);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            OnClick(e);
+
+        }
+        #endregion
+    }
+}
+
+class OvalPictureBox : PictureBox
+{
+    private bool isMouseOver = false;
+    private int originalWidth;
+    private int originalHeight;
+
+    public OvalPictureBox()
+    {
+        this.BackColor = Color.White;
+        this.Paint += OvalPictureBox_Paint;
+        this.SizeMode = PictureBoxSizeMode.Zoom;
+        this.MouseEnter += OvalPictureBox_MouseEnter;
+        this.MouseLeave += OvalPictureBox_MouseLeave;
+    }
+
+    private void OvalPictureBox_MouseEnter(object sender, EventArgs e)
+    {
+        isMouseOver = true;
+        SaveOriginalSize();
+        ScalePictureBox(1.2f);
+    }
+
+    private void OvalPictureBox_MouseLeave(object sender, EventArgs e)
+    {
+        isMouseOver = false;
+        ScalePictureBox(1.0f);
+    }
+
+    private void SaveOriginalSize()
+    {
+        originalWidth = this.Width;
+        originalHeight = this.Height;
+    }
+
+    private void ScalePictureBox(float scaleFactor)
+    {
+        int newSizeWidth = (int)(originalWidth * scaleFactor);
+        int newSizeHeight = (int)(originalHeight * scaleFactor);
+
+        using (var gp = new GraphicsPath())
+        {
+            gp.AddEllipse(new Rectangle(0, 0, newSizeWidth - 1, newSizeHeight - 1));
+            this.Region = new Region(gp);
+        }
+    }
+    private void OvalPictureBox_Paint(object sender, PaintEventArgs e)
+    {
+        if (!isMouseOver)
+        {
+            using (var gp = new GraphicsPath())
+            {
+                gp.AddEllipse(new Rectangle(0, 0, this.Width - 1, this.Height - 1));
+                this.Region = new Region(gp);
+            }
+        }
+        else
+        {
+            ScalePictureBox(1.2f);
+        }
+    }
+}
